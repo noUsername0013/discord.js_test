feb0e899a9e7d329f25e8b5eb9f11f2f5310d3c0
noUsername0013 <jimmylai921@gmail.com>
2020-12-11 20:18:13 +0800

    actually export class mutes

--- src/Database.js
+++ src/Database.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Users = void 0;
+exports.Mutes = exports.Users = void 0;
 const sequelize_1 = require("sequelize");
 const seq = new sequelize_1.Sequelize({
     dialect: 'sqlite',
@@ -11,6 +11,7 @@ class Users extends sequelize_1.Model {
 exports.Users = Users;
 class Mutes extends sequelize_1.Model {
 }
+exports.Mutes = Mutes;
 Users.init({
     id: {
         type: sequelize_1.DataTypes.STRING,
